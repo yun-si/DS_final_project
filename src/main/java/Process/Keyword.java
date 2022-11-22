8ae898ba3549245fc607543d4fe6ba91add10162
yun-si <110306079@g.nccu.edu.tw>
2022-11-22 13:34:52 +0800

    Add Keyword class

---
+++ src/main/java/Process/Keyword.java
@@ -0,0 +1,11 @@
+package Process;
+
+public class Keyword {
+	private String name;
+	private double weight;
+	
+	public Keyword(String name, double weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+}
