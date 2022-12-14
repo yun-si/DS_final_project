d0bfec49995f8a22b4eb285f3f3bcc90dc08a9da
yun-si <110306079@g.nccu.edu.tw>
2022-12-14 20:50:30 +0800

    Edit Keyword class
    add count and getCount method

--- src/main/java/Process/Keyword.java
+++ src/main/java/Process/Keyword.java
@@ -3,6 +3,7 @@ package Process;
 public class Keyword {
 	private String name;
 	private double weight;
+	private int count;
 	
 	public Keyword(String name, double weight) {
 		this.name = name;
@@ -15,5 +16,9 @@ public class Keyword {
 	
 	public double getWeight() {
 		return this.weight;
+	}
+	
+	public double getCount() {
+		return this.count;
 	}
 }
