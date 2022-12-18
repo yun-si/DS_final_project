5820d90b9669eacf67e45cc5d19a62cad39fa731
yun-si <110306079@g.nccu.edu.tw>
2022-12-18 09:42:31 +0800

    Add Translator.java
    en to zh-TW(only) need to improve

---
+++ src/main/java/Process/Translator.java
@@ -0,0 +1,43 @@
+package Process;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Translator {
+	
+	private String text;
+	
+	public Translator(String text) {
+		this.text = text;
+	}
+	
+//    public static void main(String[] args) throws IOException {
+//        String text = "Hello world!";
+//        //Translated text: Hallo Welt!
+//        System.out.println("Translated text: " + translate("en", "de", text));
+//    }
+
+    public String translate(String langFrom, String langTo) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbzm8X9vF0XAx1Rh1HrjCtQiaGBwvknt4-PvTerRjBj8FKKqxWRyt7vdEMpairS_2pJctQ/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+}
